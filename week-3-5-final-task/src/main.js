337530eab5125020da29b8e1f5e4bc7ffb1a592b
JimmyWu <gmwu185@gmail.com>
2021-06-06 22:40:35 +0800

    改導航列收閤按鈕圖示與修正 webpack THML 樣版透過動態產生結構與原有的 script src 連結重覆 (照成點按收閤按鈕會執行二次，使得選單無法收閤起來 )

--- week-3-5-final-task/src/main.js
+++ week-3-5-final-task/src/main.js
@@ -2,7 +2,7 @@ import '@babel/polyfill';
 
 /* bootstrap 5 -------------------------------------------------------------- */
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
-window.bootstrap = bootstrap; // 將 bootstrap 指向全域
+// window.bootstrap = bootstrap; // 將 bootstrap 指向全域
 
 /** bootstrap 在任何地方啟用彈出提示框
  * https://bootstrap5.hexschool.com/docs/5.0/components/popovers/
@@ -22,5 +22,3 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 /* /bootstrap 5 -------------------------------------------------------------- */
 
 import 'scss/main.scss';
-
-import 'assets/images/logo.jpg';
