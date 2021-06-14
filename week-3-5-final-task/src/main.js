f289192c534df45ace2ac9598efd589a73729a54
JimmyWu <gmwu185@gmail.com>
2021-06-14 18:50:55 +0800

    按鈕元件一般和 lx 調整圓角，並移除 HTML 中使用的 .rounded-pill 樣式

--- week-3-5-final-task/src/main.js
+++ week-3-5-final-task/src/main.js
@@ -23,23 +23,23 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 import 'scss/main.scss';
 
-import 'images/hezuo.svg';
-import 'images/icon_good01.svg';
-import 'images/icon_good02.svg';
-import 'images/icon_good03.svg';
-import 'images/icon_good04.svg';
-import 'images/icon_heart.svg';
+// import 'images/hezuo.svg';
+// import 'images/icon_good01.svg';
+// import 'images/icon_good02.svg';
+// import 'images/icon_good03.svg';
+// import 'images/icon_good04.svg';
+// import 'images/icon_heart.svg';
 import 'images/img.jpg';
-import 'images/news01.jpg';
-import 'images/news02.jpg';
-import 'images/news03.jpg';
+// import 'images/news01.jpg';
+// import 'images/news02.jpg';
+// import 'images/news03.jpg';
 import 'images/p1.jpg';
 import 'images/p2.jpg';
 import 'images/plan_img.jpg';
 import 'images/produt_image.jpg';
-import 'images/store_img.jpg';
-import 'images/user_img01.jpg';
-import 'images/user_img02.jpg';
+// import 'images/store_img.jpg';
+// import 'images/user_img01.jpg';
+// import 'images/user_img02.jpg';
 
 /* 手機版贊助專案按鈕 ---------------------------------------------------------------- */
 /** 參考資料
